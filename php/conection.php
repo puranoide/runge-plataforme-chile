22f3f2fa6adafab8400d2e136ceb0af05da891d9
puranoide <121548953+puranoide@users.noreply.github.com>
2024-07-19 09:55:14 -0500

    login-commit

---
+++ php/conection.php
@@ -0,0 +1,15 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "rungeDatabasev2";
+$conexion = mysqli_connect($servername,$username,$password,$database);
+
+//header("location:../index");
+/*
+if($conexion){
+    echo "conectado exitosamente a la base de datos";
+}else{
+    echo "no se conecto";
+}*/
+?>
