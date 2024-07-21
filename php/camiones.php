297af22d21f49c58968ee5b65b8ac5a3ef90bc74
puranoide <121548953+puranoide@users.noreply.github.com>
2024-07-20 21:47:05 -0500

    Create camiones.php
    modulo de funciones para camiones

---
+++ php/camiones.php
@@ -0,0 +1,101 @@
+<?php
+include_once('conection.php');
+
+
+function listarCamiones($con)
+{
+    $mensaje = "no hay camiones registrados";
+    $camiones = [];
+    $sqlCamiones = "SELECT* from camion;";
+    $resultcamiones = $con->query($sqlCamiones);
+
+    if ($resultcamiones->num_rows > 0) {
+        while ($rowCamiones = $resultcamiones->fetch_assoc()) {
+            $camiones[] = $rowCamiones;
+        }
+    } else {
+        return $mensaje;
+    }
+
+    return $camiones;
+}
+
+
+function agregarCamion($con, $descripcioncamion, $placa, $cubicaje)
+{
+    $mensaje = '';
+    $sqlAddCamiones = "INSERT INTO camion(descripcionCamion,placaCamion,cubicajeCamion,estado)
+                    VALUES('$descripcioncamion','$placa','$cubicaje',1)";
+
+    $ejecutar = mysqli_query($con, $sqlAddCamiones);
+
+    if ($ejecutar) {
+        $idCamion = mysqli_insert_id($con);
+        $mensaje = 'camion agregado con exito,ID del camion agregado: ' . $idCamion;
+        return $mensaje;
+    } else {
+        $mensaje = 'camion no se pudo agregar,intentelo de nuevo o contacte con soporte';
+        return $mensaje;
+    }
+}
+
+function actualizarCamion($con, $id, $descripcioncamion, $placa, $cubicaje, $estado)
+{
+    $mensaje = '';
+    $sqlUpdateCamiones = "UPDATE camion SET descripcionCamion='$descripcioncamion', placaCamion='$placa', cubicajeCamion='$cubicaje', estado='$estado'
+    WHERE camionId=$id;";
+
+    $ejecutar = mysqli_query($con, $sqlUpdateCamiones);
+
+    if ($ejecutar) {
+
+        $mensaje = 'camion actualizadi con exito,ID del camion actualizado: ' . $id;
+        return $mensaje;
+    } else {
+        $mensaje = 'camion no se pudo actualizar,intentelo de nuevo o contacte con soporte';
+        return $mensaje;
+    }
+}
+
+function listarCamionesPorId($con,$id)
+{
+    $mensaje = "no hay camiones que coinciden con este id: ".$id;
+    $camiones = [];
+    $sqlCamiones = "SELECT* from camion WHERE camionId=$id;";
+    $resultcamiones = $con->query($sqlCamiones);
+
+    if ($resultcamiones->num_rows > 0) {
+        while ($rowCamiones = $resultcamiones->fetch_assoc()) {
+            $camiones[] = $rowCamiones;
+        }
+    } else {
+        return $mensaje;
+    }
+
+    return $camiones;
+}
+
+
+
+
+
+/*
+prueba/uso de funciones
+$resultado=listarCamiones($conexion);
+
+print_r ($resultado);
+
+
+$resultado=listarCamionesPorId($conexion,1);
+echo '<pre>';
+print_r ($resultado);
+echo '</pre>';
+
+$camionagreagdo=agregarCamion($conexion,'prueba de ingreso por funcion','L1234',30);
+echo $camionagreagdo;
+
+
+$camionagreagdo=actualizarCamion($conexion,1,'prueba de ingreso por funcion-actualizada','L1234',30,2);
+echo $camionagreagdo;
+
+*/
