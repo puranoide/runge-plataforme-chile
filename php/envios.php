c3669e92aba8bddf79675c1766810c7212725139
gabriel acosta sanchez <121548953+puranoide@users.noreply.github.com>
2024-08-08 12:12:04 -0500

    Update envios.php

--- php/envios.php
+++ php/envios.php
@@ -59,6 +59,7 @@ function insertarMontoDeEnvio($con,$id){
     
 }
 function calcularMontoDelEnvio($con,$id){
+        $montoEnvio=0;
         $envio=listarEnvioPorId($con,$id);
         if($envio['idClienteFk']=='1'){
             
