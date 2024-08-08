a748b4e52b4b19c084b8a2d36876d5420e50c96b
puranoide <121548953+puranoide@users.noreply.github.com>
2024-08-08 12:16:14 -0500

    Update envios.php

--- php/envios.php
+++ php/envios.php
@@ -209,7 +209,88 @@ echo $resultado;
 */
 
 
+/*
+logica envios
 
+
+runge
+-----
+camiones varian dependiendo de agenda(cambiar realcion conductor-camion)
+
+formula:
+
+cliente1(canontex)
+camion-m3(30-50)-
+si es 30 y el viaje 1-->subclientes-sucursal-direccion=120,000
+si es 30 y es viaje 2=60,000
+si es 30 y es viaje 3=100,000
+si es 30 y es viaje 2 y es ruta colchon=170,000
+si es 30 y es viaje 3 y es ruta colchon=170,000
+
+si es sabado y es viaje 1-->sacan bono-chofer-->25,000 peoneta-->20,000
+
+si es de 50 y es el primer viaje=140,000
+si es de 50 y es el segundo viaje=70,000
+si es de 50 y es el tercer viaje=120,000
+viaje(1,2,3)
+
+sobrecargo=variable que se suma 
+
+
+cliente 2(alicomer)---tener detalla a que subcliente se ha dado
+camion-m3(30-50)-
+si es 30 -->entrega de pan-->sucursal de subcliente=120,000
+si es 30 -->retiro de harina-->no pasa nada=130,000
+si es 30 -->retiro de cajas de carton--no pasa nada=120,000
+si es 30 -->retiro de bandejas plasticas-->sucursal de subcliente=120,000
+
+si es 50 -->entrega de pan-->sucursal de subcliente=140,000
+si es 50 -->retiro de harina-->no pasa nada=150,000
+si es 50 -->retiro de cajas de carton--no pasa nada=140,000
+si es 50 -->retiro de bandejas plasticas-->sucursal de subcliente=140,000
+
+sobrecargo=variable que se suma :=0
+
+
+cliente 3 (nutrisco)
+camion-m3(30-50)-
+si es 30 y entrega subclientes-->cantidad de clientes(1-5)-->sucursales a mano=130,000
+
+si es 50 y entrega subclientes-->cantidad de clientes(1-5)-->sucursales a mano=150,000
+
+sobrecargo=variable que se suma 
+
+cliente 4 (castaño) cuando se seleccione castaño
+camion-m3(30-50)-
+
+si es 50-->numero de viaje (1-10) del cuando es 3er viaje subclientes o sucursal hay bono
+,
+--> y entrega subclientes-->cantidad de clientes(1-5)-->sucursales a mano=135,000
+
+sobrecargo=variable que se suma 
+
+
+
+conductor que tiene que ver
+cliente
+subcliente-direccion
+numero de documento(ovt-numero de factura)
+detalle de orden
+
+el le va a dar a comenzar
+cuando comienza
+cuando termina
+foto
+y el estado
+-firmada
+
+
+
+bonos salen del 3 viaje  a minorista o sucursal
+
+
+
+*/
 
 ?>
 
