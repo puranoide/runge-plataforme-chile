7147408bfa45da56b6d855e27afa399a6ef05efc
puranoide <121548953+puranoide@users.noreply.github.com>
2024-12-16 11:22:09 -0500

    Update addEnvios.php

--- php/reroute/envios/addEnvios.php
+++ php/reroute/envios/addEnvios.php
@@ -8,15 +8,9 @@ echo"</pre>";
 if(!isset($_POST["nombreruta"]))
 {
 $_POST["nombreruta"] = "tubopack";
-
-$envio=agregarEnviosV2($conexion,$_POST["codigoEnvio"],$_POST["conductorSeleccionado"],$_POST["camionSeleccionado"],$_POST["clienteSeleccionado"],$_POST["nombreruta"],$_POST["nombre"],$_POST["fechaRegistro"],$_POST["sobrecargo"],$_POST["precio"]);
-echo $envio;
-}else{
-    $envio=agregarEnviosV2($conexion,$_POST["codigoEnvio"],$_POST["conductorSeleccionado"],$_POST["camionSeleccionado"],$_POST["clienteSeleccionado"],$_POST["nombreruta"],$_POST["nombre"],$_POST["fechaRegistro"],$_POST["sobrecargo"],$_POST["precio"]);
-    echo $envio;
 }
 
-
+$envio=agregarEnviosV2($conexion,$_POST["codigoEnvio"],$_POST["conductorSeleccionado"],$_POST["camionSeleccionado"],$_POST["clienteSeleccionado"],$_POST["nombreruta"],$_POST["nombre"],$_POST["fechaRegistro"],$_POST["sobrecargo"],$_POST["precio"]);
 /*
 echo '<script> alert("envio agregado correctamente");
                 window.location="../../views/admin/enviosviews/ViewEnvios.php";
